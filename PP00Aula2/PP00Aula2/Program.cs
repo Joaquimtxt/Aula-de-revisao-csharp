e03d78c22940f0b08f693003a6f4446631398367
Joaquim <jocaq124@gmail.com>
2026-03-14 16:31:31 -0300

    Adicionado Aula 2

---
+++ PP00Aula2/PP00Aula2/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //exemplo de atributo de classe console
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        //Exemplo de método da classe console
+        Console.WriteLine("Hello, World!");
+
+        //Exemplo de método para ler entrada do usuário
+        Console.WriteLine("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        //Exibir o nome digitado pelo user
+        Console.WriteLine($"Olá {nome}");
+    }
+}
